50ae039be13534719805382b9755c4040ed7b1ca
Ishara465 <116372693+Ishara465@users.noreply.github.com>
2025-03-27 21:01:29 +0530

    Login and Register UI Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { Home, Auth, Orders, Tables, Menu } from "./pages/index.js";
 import Headers from "./components/shared/Headers.jsx";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+  const hideHeaderRouters = ["/auth"];
   return (
     <>
-      <Router>
-        <Headers />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
-        </Routes>
-      </Router>
+      {!hideHeaderRouters.includes(location.pathname) && <Headers />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
   );
 }
 
